d525cc6002850fffa18e28414f20cbe472bdf144
Neochaotics <neochaotics@pm.me>
2000-12-31 19:09:44 -0500

    feat: wakizashi hardware

--- hosts/wakizashi/hardware.nix
+++ hosts/wakizashi/hardware.nix
@@ -1,3 +1,4 @@
+{ config, ... }:
 {
   boot = {
     initrd.availableKernelModules = [
@@ -16,7 +17,27 @@
       "kvm-intel"
       "wl"
     ];
-    extraModulePackages = [ "config.boot.kernelPackages.broadcom_sta" ];
+    # extraModulePackages = [
+    #   (config.boot.kernelPackages.broadcom_sta.overrideAttrs (old: {
+    #     patches = old.patches ++ [
+    #       (builtins.fetchurl {
+    #         url = "https://raw.githubusercontent.com/archlinux/svntogit-community/5ec5b248976f84fcd7e3d7fae49ee91289912d12/trunk/012-linux517.patch";
+    #         sha256 = "df557afdb0934ed2de6ab967a350d777cbb7b53bf0b1bdaaa7f77a53102f30ac";
+    #       })
+    #     ];
+    #   }))
+    # ];
+    kernelParams = [
+      "video=LVDS-1:1280x800@60"
+    ];
   };
-  hardware.enableRedistributableFirmware = true;
+  hardware = {
+    enableRedistributableFirmware = true;
+    nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_340;
+
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+  };
 }
