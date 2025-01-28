bda3a069dc8bbe7c37a7bd4f40f52c5ec7a4f51d
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2025-01-28 17:12:31 -0500

    Update sops.nix

--- modules/nixos/system/sops.nix
+++ modules/nixos/system/sops.nix
@@ -25,9 +25,6 @@ in
         generateKey = true;
       };
       secrets = {
-        luks_key = {
-          path = "/voot/disk.key";
-        };
         "users/quinno/password" = {
           neededForUsers = true;
         };
