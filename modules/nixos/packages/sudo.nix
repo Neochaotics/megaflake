984c3348884348cd63521cafa169dbca3dca386d
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2025-01-28 17:05:37 -0500

    fix: sudo

--- modules/nixos/packages/sudo.nix
+++ modules/nixos/packages/sudo.nix
@@ -21,25 +21,6 @@ in
       sudo-rs = {
         enable = true;
         execWheelOnly = true;
-        extraRules = [
-          {
-            commands = [
-              {
-                command = "${pkgs.systemd}/bin/reboot";
-                options = [ "NOPASSWD" ];
-              }
-              {
-                command = "${pkgs.systemd}/bin/poweroff";
-                options = [ "NOPASSWD" ];
-              }
-            ];
-            groups = [ "wheel" ];
-          }
-        ];
-        extraConfig = with pkgs; ''
-          Defaults always_set_home
-          Defaults secure_path="${lib.makeBinPath [ systemd ]}:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
-        '';
       };
     };
   };
