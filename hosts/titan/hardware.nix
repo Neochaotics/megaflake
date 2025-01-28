96a9b2580736972254def7aaaba1eae427a1f05f
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2025-01-28 17:08:27 -0500

    feat: hardware

---
+++ hosts/titan/hardware.nix
@@ -0,0 +1,7 @@
+{ config, lib, pkgs,, ... }:
+
+{
+  boot.initrd.availableKernelModules = [ "hid-generic" "nvme" "xhci_pci" "ahci" "usbhid" ];
+  boot.kernelModules = [ "kvm-amd" ];
+  hardware.cpu.amd.updateMicrocode = true;
+};
