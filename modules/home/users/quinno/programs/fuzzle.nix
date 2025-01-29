6603e493abcc905da92e12887f8bad0b203ce8f6
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2025-01-28 19:19:54 -0500

    Create fuzzle.nix

---
+++ modules/home/users/quinno/programs/fuzzle.nix
@@ -0,0 +1,20 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.cm.home.users.quinno.programs.fuzzel;
+in
+{
+  options.cm.home.users.quinno.programs.fuzzel = {
+    enable = lib.mkEnableOption "Enable configuration";
+  };
+
+  config = lib.mkIf cfg.enable {
+      programs.fuzzle = {
+        enable = true;
+      };
+  };
+}
