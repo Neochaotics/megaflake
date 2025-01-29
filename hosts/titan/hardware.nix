d0f602b4fb8cb47e6b495435d57e7caf0b7bc0bb
Neochaotics <neochaotics@pm.me>
2025-01-28 19:02:12 -0500

    fix

--- hosts/titan/hardware.nix
+++ hosts/titan/hardware.nix
@@ -4,5 +4,7 @@
   boot.initrd.availableKernelModules = [ "hid-generic" "nvme" "xhci_pci" "ahci" "usbhid" ];
   boot.kernelModules = [ "kvm-amd" ];
   hardware.cpu.amd.updateMicrocode = true;
+  hardware.graphics.enable = true;
+  hardware.enableRedistributableFirmware = true;
 }
 
