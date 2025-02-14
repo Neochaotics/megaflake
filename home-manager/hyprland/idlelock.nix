3fbe966089b7adffa2bfd6e6acd0655efe9cd3cd
Neochaotics <neochaotics@pm.me>
2025-02-14 10:51:44 -0500

    feat: updates

--- home-manager/hyprland/idlelock.nix
+++ home-manager/hyprland/idlelock.nix
@@ -1,9 +1,9 @@
 { lib, config, ... }:
 let
-  cfg = config.cm.home.users.quinno.desktop.hyprland.idlelock;
+  cfg = config.ff.desktop.hyprland.idlelock;
 in
 {
-  options.cm.home.users.quinno.desktop.hyprland.idlelock = {
+  options.ff.desktop.hyprland.idlelock = {
     enable = lib.mkEnableOption "Enable";
   };
 
