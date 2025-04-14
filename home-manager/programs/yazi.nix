46734437c68b7d95bc5fff829dc588384624a460
Neochaotics <neochaotics@pm.me>
2025-04-14 14:20:48 -0400

    feat: yazi

--- home-manager/programs/yazi.nix
+++ home-manager/programs/yazi.nix
@@ -14,14 +14,18 @@ in
       enableZshIntegration = true;
       settings = {
         manager = {
-          ratio = [1 4 3]; # 1/8 width for parent, 4/8 width for current, 3/8 width for preview
+          ratio = [
+            1
+            3
+            2
+          ]; # 1/6 width for parent, 3/6 width for current, 2/6 width for preview
           sort_by = "mtime";
-          sort_sensitive=false;
-          sort_reverse=false;
-          sort_dir_first=true;
+          sort_sensitive = false;
+          sort_reverse = false;
+          sort_dir_first = true;
           linemode = "size";
           show_hidden = true;
-          show_symlink=true;
+          show_symlink = true;
           scrolloff = 12;
         };
         preview = {
@@ -29,14 +33,36 @@ in
           tab_size = 2;
           max_width = 640;
           max_height = 480;
-          image_filter="lanczos3";
+          image_filter = "lanczos3";
           image_quality = 75;
-          sixel_fraction=10;
+          sixel_fraction = 10;
         };
         input = {
           cursor_blink = true;
         };
       };
+    };
+    opener = {
+      play = [
+        {
+          run = "mpv \"$@\"";
+          orphan = true;
+          for = "unix";
+        }
+      ];
+      edit = [
+        {
+          run = "$EDITOR \"$@\"";
+          block = true;
+          for = "unix";
+        }
+      ];
+      open = [
+        {
+          run = "xdg-open \"$@\"";
+          desc = "Openz";
+        }
+      ];
     };
   };
 }
