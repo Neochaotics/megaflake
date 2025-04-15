46f1ce3f7b024a0a9b4126ed5d21a701e70ad5a7
Neochaotics <neochaotics@pm.me>
2025-04-15 16:27:34 -0400

    fix: hyprland monitor pos

--- home-manager/hyprland/monitors.nix
+++ home-manager/hyprland/monitors.nix
@@ -2,9 +2,9 @@
 {
   wayland.windowManager.hyprland.settings = lib.mkIf config.qm.desktop.hyprland.enable {
     monitor = [
-      "DP-2, 2560x1440@120, auto, 1"
-      "DP-1,1920x1080@60, auto-right, 1, transform, 3"
-      "HDMI-A-1, 1920x1080@60, auto-up, 1"
+      "DP-2, 2560x1440@120, 0x0, 1"
+      "DP-1,1920x1080@60, 2560x240, 1, transform, 3"
+      "HDMI-A-1, 1920x1080@60, 1920,320, 1"
     ];
 
     workspace = [
