156562d9cbc6d732e9d396ad24eedb50b699a05d
Neochaotics <neochaotics@pm.me>
2025-04-15 17:10:27 -0400

    feat: gitui

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -14,34 +14,37 @@ in
 
   config = lib.mkIf cfg.enable {
     home.packages = [ pkgs.gh ];
+    programs = {
 
-    programs.git = {
-      enable = true;
+      gitui.enable = true;
+      git = {
+        enable = true;
 
-      userName = "Neochaotics";
-      userEmail = "neochaotics@pm.me";
+        userName = "Neochaotics";
+        userEmail = "neochaotics@pm.me";
 
-      extraConfig = {
-        core = {
-          editor = "nvim";
+        extraConfig = {
+          core = {
+            editor = "nvim";
+          };
+          color = {
+            diff = "auto";
+            interactive = "auto";
+            pager = "true";
+            status = "auto";
+            branch = "auto";
+            ui = "true";
+          };
+          rerere = {
+            enabled = "true";
+            autoupdate = "true";
+          };
+          rebase = {
+            autoSquash = "true";
+          };
+          push.default = "upstream";
+          pull.rebase = "true";
         };
-        color = {
-          diff = "auto";
-          interactive = "auto";
-          pager = "true";
-          status = "auto";
-          branch = "auto";
-          ui = "true";
-        };
-        rerere = {
-          enabled = "true";
-          autoupdate = "true";
-        };
-        rebase = {
-          autoSquash = "true";
-        };
-        push.default = "upstream";
-        pull.rebase = "true";
       };
     };
   };
