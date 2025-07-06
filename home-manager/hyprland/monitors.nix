d909b83bd9fe10647ef91d8d88d6bab6006cb673
Neochaotics <neochaotics@pm.me>
2025-07-06 12:39:12 -0400

    feat: displaySpec v2

--- home-manager/hyprland/monitors.nix
+++ home-manager/hyprland/monitors.nix
@@ -7,7 +7,8 @@
     #];
 
     monitor = lib.mapAttrsToList (
-      name: cfg: "${name}, ${toString cfg.resolution.width}x${toString cfg.resolution.height}, auto, 1"
+      name: cfg:
+      "${name}, ${toString cfg.resolution.width}x${toString cfg.resolution.height}@${toString cfg.framerate}, ${toString cfg.position.x}x${toString cfg.position.y}, ${toString cfg.scale}"
     ) config.ff.hardware.videoPorts;
 
     workspace = [
