2e9af881fe7179beb9afe329971030dcf14f052f
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2025-08-10 18:44:35 -0400

    fix: fmt

--- hosts/wakizashi/default.nix
+++ hosts/wakizashi/default.nix
@@ -5,22 +5,19 @@
   pkgs,
   self,
   ...
-}:
-let
+}: let
   username = "quinno";
-  formatUsername =
-    name:
+  formatUsername = name:
     lib.strings.stringAsChars (
       c:
-      if c == builtins.substring ((builtins.stringLength name) - 1) 1 name then
-        " ${lib.strings.toUpper c}"
-      else if c == (builtins.substring 0 1 name) then
-        lib.strings.toUpper c
-      else
-        c
-    ) name;
-in
-{
+        if c == builtins.substring ((builtins.stringLength name) - 1) 1 name
+        then " ${lib.strings.toUpper c}"
+        else if c == (builtins.substring 0 1 name)
+        then lib.strings.toUpper c
+        else c
+    )
+    name;
+in {
   imports = [
     inputs.ff.nixosModules.freedpomFlake
     inputs.qm.nixosModules.qModule
@@ -35,7 +32,7 @@ in
 
   age = {
     rekey = {
-      masterIdentities = [ "/persist/age.key" ];
+      masterIdentities = ["/persist/age.key"];
       localStorageDir = "${self}" + "/secrets/rekeyed/${config.networking.hostName}";
       generatedSecretsDir = "${self}" + "/secrets/generated/${config.networking.hostName}";
       storageMode = "local";
@@ -51,12 +48,13 @@ in
       initialPassword = "password";
       shell = pkgs.zsh;
       ignoreShellProgramCheck = true;
-      extraGroups = [
-        "wheel"
-      ]
-      ++ lib.optional config.security.rtkit.enable "rtkit"
-      ++ lib.optional config.services.pipewire.enable "audio"
-      ++ lib.optional config.hardware.i2c.enable "i2c";
+      extraGroups =
+        [
+          "wheel"
+        ]
+        ++ lib.optional config.security.rtkit.enable "rtkit"
+        ++ lib.optional config.services.pipewire.enable "audio"
+        ++ lib.optional config.hardware.i2c.enable "i2c";
     };
     mutableUsers = lib.mkForce true;
   };
@@ -64,7 +62,7 @@ in
   services.getty.autologinUser = "${username}";
   home-manager = {
     users.${username} = import ./home.nix;
-    extraSpecialArgs = { inherit username; };
+    extraSpecialArgs = {inherit username;};
   };
 
   # System Configuration
