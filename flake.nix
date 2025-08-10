4b277ccf928c25431d9c374f36c2ce758ed0d119
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2025-08-10 13:11:37 -0400

    build(inputs): add preservation

--- flake.nix
+++ flake.nix
@@ -36,6 +36,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     impermanence.url = "github:nix-community/impermanence";
+    preservation.url = "github:nix-community/preservation";
     disko = {
       url = "github:nix-community/disko/latest";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -149,6 +150,7 @@
 
                 # Core system modules
                 inputs.impermanence.nixosModules.impermanence
+                inputs.preservation.nixosModules.preservation
                 inputs.home-manager.nixosModules.home-manager
 
                 # Security modules
