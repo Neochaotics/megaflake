ccac4e8c361abaa56bc327cd1d4851f0be0bf3b3
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2025-08-16 13:06:55 -0400

    fix(hypr): disable uwsm

--- nixos/programs/hyprland.nix
+++ nixos/programs/hyprland.nix
@@ -11,10 +11,10 @@ in {
 
   config = lib.mkIf cfg.enable {
     programs = {
-      uwsm.enable = true;
+      uwsm.enable = false;
       hyprland = {
         enable = true;
-        withUWSM = true;
+        withUWSM = false;
         #package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
         #portalPackage =
         #  inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
