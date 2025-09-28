32baac5cda4eba2fd46152bb406d9b1183c2f220
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2025-09-28 18:32:40 -0400

    chore(update): flake.lock

--- hosts/nodachi/default.nix
+++ hosts/nodachi/default.nix
@@ -3,19 +3,22 @@
   config,
   pkgs,
   ...
-}: let
+}:
+let
   username = "quinno";
-  formatUsername = name:
+  formatUsername =
+    name:
     lib.strings.stringAsChars (
       c:
-        if c == builtins.substring ((builtins.stringLength name) - 1) 1 name
-        then " ${lib.strings.toUpper c}"
-        else if c == (builtins.substring 0 1 name)
-        then lib.strings.toUpper c
-        else c
-    )
-    name;
-in {
+      if c == builtins.substring ((builtins.stringLength name) - 1) 1 name then
+        " ${lib.strings.toUpper c}"
+      else if c == (builtins.substring 0 1 name) then
+        lib.strings.toUpper c
+      else
+        c
+    ) name;
+in
+{
   imports = [
     ./disk-primary.nix
     ./disk-secondary.nix
@@ -40,13 +43,12 @@ in {
       initialPassword = "password";
       shell = pkgs.zsh;
       ignoreShellProgramCheck = true;
-      extraGroups =
-        [
-          "wheel"
-        ]
-        ++ lib.optional config.security.rtkit.enable "rtkit"
-        ++ lib.optional config.services.pipewire.enable "audio"
-        ++ lib.optional config.hardware.i2c.enable "i2c";
+      extraGroups = [
+        "wheel"
+      ]
+      ++ lib.optional config.security.rtkit.enable "rtkit"
+      ++ lib.optional config.services.pipewire.enable "audio"
+      ++ lib.optional config.hardware.i2c.enable "i2c";
     };
     mutableUsers = lib.mkForce true;
   };
@@ -54,10 +56,14 @@ in {
   services.getty.autologinUser = "${username}";
   home-manager = {
     users.${username} = import ./home.nix;
-    extraSpecialArgs = {inherit username;};
+    extraSpecialArgs = { inherit username; };
   };
 
   services.tailscale.enable = true;
+
+  services.pipewire.enable = lib.mkForce true;
+
+  services.flatpak.enable = true;
 
   ff = {
     common.enable = true;
