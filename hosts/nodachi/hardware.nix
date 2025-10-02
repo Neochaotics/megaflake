ef1d915acfd966547645d95ae50a8c2972827d5b
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2025-10-02 01:28:41 -0400

    feat: better stuff

--- hosts/nodachi/hardware.nix
+++ hosts/nodachi/hardware.nix
@@ -3,16 +3,51 @@
   pkgs,
   ...
 }: {
-  # ff.hardware.displays = {
-  #   test = {
-  #     port = "DP-2";
-  #     refreshRate = 144;
-  #     resWidth = 2560;
-  #     resHeight = 1440;
-  #     ownedWorkspaces = [ 1 ];
+  # ff.hardware.videoPorts = {
+  #   "DP-1" = {
+  #     resolution = {
+  #       width = 2560;
+  #       height = 1440;
+  #     };
+  #     framerate = 180;
+  #     vrr = 3;
+  #     cm = "auto";
+  #     colorDepth = 10;
+  #     workspaces = [
+  #       "1"
+  #       "2"
+  #       "3"
+  #       "4"
+  #     ];
+  #   };
+  #   "DP-5" = {
+  #     resolution = {
+  #       width = 1920;
+  #       height = 1080;
+  #     };
+  #     framerate = 75;
+  #     position = "auto-center-right";
+  #     transform = 3;
+  #     workspaces = [
+  #       "5"
+  #       "6"
+  #       "7"
+  #       "8"
+  #     ];
+  #   };
+  #   "HDMI-A-2" = {
+  #     resolution = {
+  #       width = 3840;
+  #       height = 2160;
+  #     };
+  #     scale = 2;
+  #     position = "auto-center-left";
+  #     workspaces = [
+  #       "9"
+  #       "10"
+  #     ];
   #   };
   # };
-  #
 
   boot = {
     kernelPackages = pkgs.linuxPackages_cachyos;
--- hosts/nodachi/home.nix
+++ hosts/nodachi/home.nix
@@ -25,51 +25,6 @@
     programs = {
       bash.enable = true;
     };
-    hardware.videoPorts = {
-      "DP-1" = {
-        resolution = {
-          width = 2560;
-          height = 1440;
-        };
-        framerate = 180;
-        vrr = 3;
-        cm = "auto";
-        colorDepth = 10;
-        workspaces = [
-          "1"
-          "2"
-          "3"
-          "4"
-        ];
-      };
-      "DP-5" = {
-        resolution = {
-          width = 1920;
-          height = 1080;
-        };
-        framerate = 75;
-        position = "auto-center-right";
-        transform = 3;
-        workspaces = [
-          "5"
-          "6"
-          "7"
-          "8"
-        ];
-      };
-      "HDMI-A-2" = {
-        resolution = {
-          width = 3840;
-          height = 2160;
-        };
-        scale = 2;
-        position = "auto-center-left";
-        workspaces = [
-          "9"
-          "10"
-        ];
-      };
-    };
   };
 
   qm = {
