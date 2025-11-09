ac93024db4e325086c6e04f67c1b3b310fabc275
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2025-11-09 03:37:28 -0500

    feat: no wg0

--- modules/nixos/wireguard.nix
+++ modules/nixos/wireguard.nix
@@ -11,19 +11,19 @@ in {
 
   config = lib.mkIf cfg.enable {
     networking.wg-quick.interfaces = {
-      wg0 = {
-        address = ["10.2.0.2/32"];
-        dns = ["10.2.0.1"];
-        privateKeyFile = "/persist/wg0.key";
+      #wg0 = {
+      #  address = ["10.2.0.2/32"];
+      #  dns = ["10.2.0.1"];
+      #  privateKeyFile = "/persist/wg0.key";
 
-        peers = [
-          {
-            publicKey = "tHwmpVZsh4yfoA9/vWbacF6cWcXUKE9wuDP5bz66oh8=";
-            allowedIPs = ["0.0.0.0/0"];
-            endpoint = "138.199.50.107:51820";
-          }
-        ];
-      };
+      #  peers = [
+      #    {
+      #      publicKey = "tHwmpVZsh4yfoA9/vWbacF6cWcXUKE9wuDP5bz66oh8=";
+      #      allowedIPs = ["0.0.0.0/0"];
+      #      endpoint = "138.199.50.107:51820";
+      #    }
+      #  ];
+      #};
       wgBM = {
         address = ["10.1.4.3/32"];
         dns = ["10.0.0.1"];
